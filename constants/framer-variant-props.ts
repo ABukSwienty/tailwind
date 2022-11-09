371dbc2c2cde45144de58aa2e8f2bfe8467c7d25
ABukSwienty <alexanderbukswienty@gmail.com>
2022-11-09 22:41:41 +0100

    feat(framer props)

---
+++ constants/framer-variant-props.ts
@@ -0,0 +1,12 @@
+const framerVariantProps = {
+  initial: "initial",
+  animate: "animate",
+  exit: "exit",
+  whileHover: "hover",
+  whileTap: "tap",
+  whileDrag: "drag",
+  whileInView: "inView",
+  whileFocus: "focus",
+};
+
+export default framerVariantProps;
