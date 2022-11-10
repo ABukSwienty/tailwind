a0fe50efa0304c58e7841498395bf7e1f5562523
ABukSwienty <alexanderbukswienty@gmail.com>
2022-11-10 09:50:58 +0100

    feat(organism): landing

---
+++ components/organisms/landing.tsx
@@ -0,0 +1,25 @@
+import Section from "../atoms/section";
+import TextHighlight from "../atoms/text-highlight";
+import Title from "../atoms/title";
+
+const Landing = () => (
+  <Section
+    className="flex min-h-screen flex-col justify-end px-4 py-8 font-black text-white"
+    color="brand"
+  >
+    <Title
+      size="5xl"
+      className="transition-all duration-150 ease-in-out xs:text-7xl sm:text-8xl lg:text-9xl"
+    >
+      <TextHighlight text="turn" mode="italic" /> HEADWIND
+    </Title>
+    <Title
+      size="5xl"
+      className="transition-all duration-150 ease-in-out xs:text-7xl sm:text-8xl lg:text-9xl"
+    >
+      <TextHighlight text="into" mode="italic" /> TAILWIND
+    </Title>
+  </Section>
+);
+
+export default Landing;
